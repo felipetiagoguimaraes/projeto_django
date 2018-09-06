863dc14dcd8463dea70e193369ff2b8f69e19ebb
felipetiagoguimaraes <felipeguimaraes0025@gmail.com>
2018-09-06 10:13:30 -0300

    Modificação models e about com inclusão de imagens

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,19 +1,12 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
-def index(request):
-    context_dict = {}
-    context_dict['boldmessage'] = "Olá BSI6, este texto veio da view!"
-    context_dict["autor"] = "Felipe Tiago Guimarães"
 
+def index(request):
+    context_dict = {'boldmessage': "Olá BSI6, este texto veio da view!"}
     return render(request, 'rango/index.html', context=context_dict)
 
+
 def about(request):
-    texto = '''
-    <h1> Sobre o Rango </h1>
-    Essa é a página sobre o Rango.
-
-    <br/><a href='/rango/'>Index</a>
-    '''
-    return HttpResponse(texto)
-
+    context_dict = {'autor': 'Felipe Tiago Guimarães'}
+    return render(request, 'rango/about.html', context=context_dict)
