978b1b709f96374e63240c9a2ade3a32d80bc6f1
felipetiagoguimaraes <felipeguimaraes0025@gmail.com>
2018-09-13 10:16:26 -0300

    popular categorias

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from rango.models import Category, Page
-admin.site.register(Category)
-admin.site.register(Page)
+
+class PageAdmin(admin.ModelAdmin):
+    # list_display = ('category', 'title', 'url')
+    list_display = ('category', 'title', 'views')
+    list_filter = ('category',)
+    ordering = ('views', 'category', 'title')
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
+# admin.site.register(Page)
